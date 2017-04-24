5538ec7f343c4fd794348ef5238859113b0d727c
Royyyy <410310446@qq.com>
2017-04-24 12:08:37 +0800

    Initial commit

---
+++ app/src/main/java/com/roy/mobilesafe/SplashActivity.java
@@ -0,0 +1,90 @@
+package com.roy.mobilesafe;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView textView;
+    private  int mLocalVersionCode;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //初始化UI
+        initUi();
+        //初始化数据
+        initData();
+    }
+
+    /**
+     * 初始化数据方法
+     */
+    private void initData() {
+        //1.应用版本名称
+        textView.setText("版本的名称；"+getVersionName());
+        //检测(本地版本号和服务器版本号对比)是否有更新，如果有更新，提示用户下载(member)
+        //2.获取本地版本号
+        mLocalVersionCode = getVersionCode();
+        //3.获取服务器版本号(客户端发请求，服务器给相应(Json,xml))
+        //http://www.oxxx.com/update.json?key=value 返回200请求成功，流的方式将数据读取下来
+        //Json中内容包括
+        /*
+        更新版本的版本名称
+        新版本的描述信息
+        服务器版本号
+        新版本apk下载地址
+         */
+    }
+
+    /**
+     * 初始化UI方法
+     */
+    private void initUi() {
+        textView = (TextView) findViewById(R.id.tv_version_name);
+
+    }
+
+
+
+    /**
+     * 获取版本名称：清单文件中
+     * @return
+     */
+    private String getVersionName() {
+        //1.包管理者对象packageManager
+        PackageManager packageManager = getPackageManager();
+        //2.从包的管理者对象中,获取指定包名的基本信息(版本名称，版本号)，传0代表获取基本信息
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(),0);
+            return packageInfo.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * 返回版本号
+     * @return
+     * 非0则证明返回成功
+     */
+    public int getVersionCode() {
+        //1.包管理者对象packageManager
+        PackageManager packageManager = getPackageManager();
+        //2.从包的管理者对象中,获取指定包名的基本信息(版本名称，版本号)，传0代表获取基本信息
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(),0);
+            return packageInfo.versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
